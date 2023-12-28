fec8c8f34ff79c8bc405482ed511216ae8787eb9
chat43jkav <153569213+chat43jkav@users.noreply.github.com>
2023-12-27 18:18:17 -0600

    Add files via upload

---
+++ init.js
@@ -0,0 +1,11 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDB9KfXrdb9lphd4HjSmMpjK5tpbHOm1Zk",
+    authDomain: "chatalonsoooo.firebaseapp.com",
+    projectId: "chatalonsoooo",
+    storageBucket: "chatalonsoooo.appspot.com",
+    messagingSenderId: "214868544081",
+    appId: "1:214868544081:web:0ee0604d6d04e74a228670"
+    });
